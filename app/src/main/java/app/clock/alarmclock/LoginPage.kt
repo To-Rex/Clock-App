5734e8c9f2aee36219210b97b1e09238e357adc7
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-30 03:26:18 +0500

    Calc Application Application

--- app/src/main/java/app/clock/alarmclock/LoginPage.kt
+++ app/src/main/java/app/clock/alarmclock/LoginPage.kt
@@ -6,6 +6,9 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import app.clock.alarmclock.cleint.ApiCleint
+import app.clock.alarmclock.models.LoginModels
+import retrofit2.Call
 
 class LoginPage : AppCompatActivity() {
 
@@ -34,12 +37,36 @@ class LoginPage : AppCompatActivity() {
             } else if (password.isEmpty()) {
                 ediLogPas?.error = "Please enter password"
             } else {
-                if (email.length < 6){
+                if (email.length < 6) {
                     ediLogEmail?.error = "Email must be at least 6 characters"
-                } else if (password.length < 4){
+                } else if (password.length < 4) {
                     ediLogPas?.error = "Password must be at least 5 characters"
                 } else {
-                    Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                    val loginModels = LoginModels(email, password, "")
+                    val user: Call<LoginModels> = ApiCleint().userService.login(loginModels)
+                    user.enqueue(object : retrofit2.Callback<LoginModels> {
+                        override fun onResponse(
+                            call: Call<LoginModels>,
+                            response: retrofit2.Response<LoginModels>
+                        ) {
+                            if (response.isSuccessful) {
+                                Toast.makeText(
+                                    this@LoginPage,
+                                    "Login Successful",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                println(response.body())
+                            } else {
+                                Toast.makeText(this@LoginPage, "Login Failed", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+
+                        override fun onFailure(call: Call<LoginModels>, t: Throwable) {
+                            Toast.makeText(this@LoginPage, "Login Failed", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    })
                 }
             }
         }
@@ -49,7 +76,4 @@ class LoginPage : AppCompatActivity() {
         }
     }
 
-    private fun Login(){
-
-    }
 }
