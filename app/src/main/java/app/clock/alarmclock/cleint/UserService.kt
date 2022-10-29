05da61194171f6d30c53cde4c606b252773eda2e
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-30 04:37:31 +0500

    Calc Application Application

---
+++ app/src/main/java/app/clock/alarmclock/cleint/UserService.kt
@@ -0,0 +1,11 @@
+package app.clock.alarmclock.cleint
+
+import retrofit2.http.POST
+import app.clock.alarmclock.models.LoginModels
+import retrofit2.Call
+import retrofit2.http.Body
+
+interface UserService {
+    @POST("login")
+    fun login(@Body loginModels: LoginModels?): Call<Any?>?
+}
---
+++ app/src/main/java/app/clock/alarmclock/models/LoginModels.kt
@@ -0,0 +1,5 @@
+package app.clock.alarmclock.models
+
+class LoginModels(var email: String, var password: String) {
+    var token: String? = null
+}
