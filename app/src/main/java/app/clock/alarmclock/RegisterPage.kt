6ff8423c39011e3418906e5a57b6883ac3271b27
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-30 02:27:54 +0500

    Calc Application Application

--- app/src/main/java/app/clock/alarmclock/RegisterPage.kt
+++ app/src/main/java/app/clock/alarmclock/RegisterPage.kt
@@ -1,11 +1,24 @@
 package app.clock.alarmclock
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class RegisterPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_page)
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                val intent = Intent(this, LoginPage::class.java)
+                startActivity(intent)
+                finish()
+            },
+            3000
+        )
+
     }
 }
