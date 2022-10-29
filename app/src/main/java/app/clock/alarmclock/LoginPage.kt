59ab40ba46dde1a4b4042cb7f4a236f5f4c7f206
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-30 03:17:30 +0500

    Calc Application Application

--- app/src/main/java/app/clock/alarmclock/LoginPage.kt
+++ app/src/main/java/app/clock/alarmclock/LoginPage.kt
@@ -2,11 +2,30 @@ package app.clock.alarmclock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class LoginPage : AppCompatActivity() {
+
+    var ediLogEmail: EditText? = null
+    var ediLogPas: EditText? = null
+    var txtLogPass: TextView? = null
+    var btnLogLogin: Button? = null
+    var txtLogReg: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_login_page)
+
+        ediLogEmail = findViewById(R.id.ediLogEmail)
+        ediLogPas = findViewById(R.id.ediLogPas)
+        txtLogPass = findViewById(R.id.txtLogPass)
+        btnLogLogin = findViewById(R.id.btnLogLogin)
+        txtLogReg = findViewById(R.id.txtLogReg)
+
+        
+
     }
 }
