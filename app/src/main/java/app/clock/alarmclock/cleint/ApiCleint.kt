5842c1de64b3cdc4ad879e3d40f5b3cf5772a457
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-30 02:33:14 +0500

    Calc Application Application

--- app/src/main/java/app/clock/alarmclock/cleint/ApiCleint.kt
+++ app/src/main/java/app/clock/alarmclock/cleint/ApiCleint.kt
@@ -1,4 +1,15 @@
 package app.clock.alarmclock.cleint
 
 class ApiCleint {
+    private fun getRetrofit(): Retrofit {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+        return Retrofit.Builder()
+            .baseUrl("https://api.astrocoin.uz/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+    val userService: UserService get() = getRetrofit().create(UserService::class.java)
 }
---
+++ app/src/main/java/app/clock/alarmclock/cleint/UserService.java
@@ -0,0 +1,4 @@
+package app.clock.alarmclock.cleint;
+
+public interface UserService {
+}
