b04d31f45d063cedd7a91495fc30bedc23374078
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-30 02:36:16 +0500

    Calc Application Application

--- app/src/main/java/app/clock/alarmclock/models/LoginModels.java
+++ app/src/main/java/app/clock/alarmclock/models/LoginModels.java
@@ -5,4 +5,33 @@ public class LoginModels {
     String password;
     String token;
 
+    public LoginModels(String email, String password, String token) {
+        this.email = email;
+        this.password = password;
+        this.token = token;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
