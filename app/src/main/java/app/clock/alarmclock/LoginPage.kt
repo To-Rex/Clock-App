c9f11bbd81b03960f146349b31f33465f503f3e3
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-30 03:22:47 +0500

    Calc Application Application

--- app/src/main/java/app/clock/alarmclock/LoginPage.kt
+++ app/src/main/java/app/clock/alarmclock/LoginPage.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class LoginPage : AppCompatActivity() {
 
@@ -25,7 +26,30 @@ class LoginPage : AppCompatActivity() {
         btnLogLogin = findViewById(R.id.btnLogLogin)
         txtLogReg = findViewById(R.id.txtLogReg)
 
-        
+        btnLogLogin?.setOnClickListener {
+            val email = ediLogEmail?.text.toString()
+            val password = ediLogPas?.text.toString()
+            if (email.isEmpty()) {
+                ediLogEmail?.error = "Please enter email"
+            } else if (password.isEmpty()) {
+                ediLogPas?.error = "Please enter password"
+            } else {
+                if (email.length < 6){
+                    ediLogEmail?.error = "Email must be at least 6 characters"
+                } else if (password.length < 4){
+                    ediLogPas?.error = "Password must be at least 5 characters"
+                } else {
+                    Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        txtLogReg?.setOnClickListener {
+            Toast.makeText(this, "Register", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun Login(){
 
     }
 }
--- app/src/main/java/app/clock/alarmclock/cleint/UserService.kt
+++
@@ -1,3 +0,0 @@
-package app.clock.alarmclock.cleint
-
-interface UserService 
