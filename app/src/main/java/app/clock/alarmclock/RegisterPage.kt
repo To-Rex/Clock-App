f1c3a91116d8055bf136809c46ccafbb205cf104
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-30 02:25:45 +0500

    Calc Application Application

---
+++ app/src/main/java/app/clock/alarmclock/RegisterPage.kt
@@ -0,0 +1,11 @@
+package app.clock.alarmclock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_page)
+    }
+}
