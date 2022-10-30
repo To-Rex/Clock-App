9cab4acf45c1e6a3b916390154995e473d48bc90
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-30 12:30:48 +0500

    Calc Application Application

--- app/src/main/java/app/clock/alarmclock/RegisterPage.kt
+++ app/src/main/java/app/clock/alarmclock/RegisterPage.kt
@@ -5,8 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class RegisterPage : AppCompatActivity() {
+
+    private var ediRegEmail: EditText? = null
+    private var ediRegPas: EditText? = null
+    private var ediRegRePas: EditText? = null
+    private var btnRegOk: Button? = null
+    private var txtRegEnter: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
