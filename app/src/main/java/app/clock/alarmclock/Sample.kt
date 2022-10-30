26d0d81a8dd0865d90225d826e05296162ce1b2e
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-30 17:54:35 +0500

    Calc Application Application

--- app/src/main/java/app/clock/alarmclock/Sample.kt
+++ app/src/main/java/app/clock/alarmclock/Sample.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class Sample : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_sample)
     }
 }
