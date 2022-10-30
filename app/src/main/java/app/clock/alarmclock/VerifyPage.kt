02269752c2de87933b01122f8a86aaca08c56bd7
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-30 13:32:49 +0500

    Calc Application Application

--- app/src/main/java/app/clock/alarmclock/VerifyPage.kt
+++ app/src/main/java/app/clock/alarmclock/VerifyPage.kt
@@ -1,11 +1,26 @@
 package app.clock.alarmclock
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class VerifyPage : AppCompatActivity() {
+
+    private var ediVerCode: EditText? = null
+    private var txtVerTime: TextView? = null
+    private var btnVerOk: Button? = null
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verify_page)
+
+        ediVerCode = findViewById(R.id.ediVerCode)
+        txtVerTime = findViewById(R.id.txtVerTime)
+        btnVerOk = findViewById(R.id.btnVerOk)
     }
 }
