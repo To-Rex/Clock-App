9775d7ce892cddab3f0b053e81daebdc3f2b3833
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-30 19:11:33 +0500

    Calc Application Application

--- app/src/main/java/app/clock/alarmclock/adapters/DataAdapters.kt
+++ app/src/main/java/app/clock/alarmclock/adapters/DataAdapters.kt
@@ -1,23 +1,39 @@
 package app.clock.alarmclock.adapters
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
 import android.view.View
 import android.widget.BaseAdapter
 import android.view.ViewGroup
+import android.widget.TextView
+import app.clock.alarmclock.R
+import app.clock.alarmclock.models.GetTimes
 
-class DataAdapters : BaseAdapter() {
+class DataAdapters(private val context: Activity, private val list:ArrayList<GetTimes>) : BaseAdapter() {
     override fun getCount(): Int {
-        return 0
+        return list.size
     }
 
     override fun getItem(position: Int): Any {
-        return 0
+        return list[position]
     }
 
     override fun getItemId(position: Int): Long {
-        return 0
+        return position.toLong()
     }
 
-    override fun getView(position: Int, convertView: View, parent: ViewGroup): View {
-        return View(parent.context)
+    @SuppressLint("ViewHolder", "InflateParams", "MissingInflatedId")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view = context.layoutInflater.inflate(R.layout.times_list, null)
+
+        val times = view.findViewById<View>(R.id.txtTimes) as TextView
+        val txtComents = view.findViewById<View>(R.id.txtComent) as TextView
+        val getTimes = list[position]
+        times.text = getTimes.times
+        txtComents.text = getTimes.coments
+
+        return view
     }
+
 }
