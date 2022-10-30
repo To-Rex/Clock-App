73347146b28a9b08f3c0829244c560b99e828751
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-30 18:38:59 +0500

    Calc Application Application

---
+++ app/src/main/java/app/clock/alarmclock/SettingsPage.kt
@@ -0,0 +1,11 @@
+package app.clock.alarmclock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_page)
+    }
+}
