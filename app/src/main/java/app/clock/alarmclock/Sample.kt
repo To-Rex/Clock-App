061e01cf452bdb7d93f29e8dbfc836919b5da931
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-30 19:26:12 +0500

    Calc Application Application

--- app/src/main/java/app/clock/alarmclock/Sample.kt
+++ app/src/main/java/app/clock/alarmclock/Sample.kt
@@ -4,8 +4,11 @@ import android.annotation.SuppressLint
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ListView
+import android.widget.ProgressBar
 import android.widget.Toast
+import app.clock.alarmclock.adapters.DataAdapters
 import app.clock.alarmclock.cleint.ApiCleint
 import app.clock.alarmclock.models.GetTimes
 import com.google.gson.Gson
@@ -17,6 +20,10 @@ class Sample : AppCompatActivity() {
     private var sharedPreferences: SharedPreferences? = null
     var token = ""
     var listSample: ListView? = null
+    private var dataAdapters: DataAdapters? = null
+    private var timeList: ArrayList<GetTimes>? = null
+    private var progressSampe: ProgressBar? = null
+    private var isLoading: Boolean = false
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,15 +32,28 @@ class Sample : AppCompatActivity() {
         setContentView(R.layout.activity_sample)
 
         listSample = findViewById(R.id.listSample)
+        progressSampe = findViewById(R.id.progressSampe)
 
         sharedPreferences = getSharedPreferences("app.clock.alarmClock", MODE_PRIVATE)
         token = sharedPreferences?.getString("token", "")!!
+
+        timeList = ArrayList()
+        dataAdapters = DataAdapters(this, timeList!!)
+        listSample?.adapter = dataAdapters
+        listSample?.divider = null
+        listSample?.dividerHeight = 20
+
+        getAllTimes()
+
+    }
+    private fun getAllTimes() {
         val getTimes = GetTimes("","","")
         val gettime: Call<Any?>? = ApiCleint().userService.gettimes("Bearer $token")
         gettime?.enqueue(object : retrofit2.Callback<Any?> {
             override fun onResponse(call: Call<Any?>, response: retrofit2.Response<Any?>) {
                 if (response.isSuccessful) {
-                    Toast.makeText(this@Sample, "Success", Toast.LENGTH_SHORT).show()
+                    listSample?.visibility = View.VISIBLE
+                    progressSampe?.visibility = View.GONE
                     val gson = Gson()
                     val json = gson.toJson(response.body())
                     val jsonObject = JSONObject(json)
@@ -41,10 +61,14 @@ class Sample : AppCompatActivity() {
                     val switchs = jsonObject.getJSONArray("switchs")
                     val coments = jsonObject.getJSONArray("coments")
                     for (i in 0 until times.length()) {
-                        getTimes.times = times.getString(i)
-                        getTimes.switchs = switchs.getString(i)
-                        getTimes.coments = coments.getString(i)
-                        Toast.makeText(this@Sample, getTimes.times, Toast.LENGTH_SHORT).show()
+                        val time = times.getString(i)
+                        val switch = switchs.getString(i)
+                        val coment = coments.getString(i)
+                        getTimes.times = time
+                        getTimes.switchs = switch
+                        getTimes.coments = coment
+                        timeList?.add(getTimes)
+                        dataAdapters?.notifyDataSetChanged()
                     }
                 }else{
                     Toast.makeText(this@Sample, "Error", Toast.LENGTH_SHORT).show()
