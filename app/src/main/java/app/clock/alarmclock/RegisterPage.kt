e577d92cf32ae0976aa9f8b442906ce8e67d7eb5
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-30 12:33:53 +0500

    Calc Application Application

--- app/src/main/java/app/clock/alarmclock/RegisterPage.kt
+++ app/src/main/java/app/clock/alarmclock/RegisterPage.kt
@@ -22,5 +22,42 @@ class RegisterPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_page)
 
+        ediRegEmail = findViewById(R.id.ediRegEmail)
+        ediRegPas = findViewById(R.id.ediRegPas)
+        ediRegRePas = findViewById(R.id.ediRegRePas)
+        btnRegOk = findViewById(R.id.btnRegOk)
+        txtRegEnter = findViewById(R.id.txtRegEnter)
+
+        btnRegOk?.setOnClickListener {
+            val email = ediRegEmail?.text.toString()
+            val password = ediRegPas?.text.toString()
+            val rePassword = ediRegRePas?.text.toString()
+            if (email.isEmpty()){
+                ediRegEmail?.error = "Email is required"
+                ediRegEmail?.requestFocus()
+                return@setOnClickListener
+            }
+            if (password.isEmpty()){
+                ediRegPas?.error = "Password is required"
+                ediRegPas?.requestFocus()
+                return@setOnClickListener
+            }
+            if (rePassword.isEmpty()){
+                ediRegRePas?.error = "Re-Password is required"
+                ediRegRePas?.requestFocus()
+                return@setOnClickListener
+            }
+            if (password != rePassword){
+                ediRegRePas?.error = "Password not match"
+                ediRegRePas?.requestFocus()
+                return@setOnClickListener
+            }
+            if (password.length < 4){
+                ediRegPas?.error = "Password must be at least 6 characters"
+                ediRegPas?.requestFocus()
+                return@setOnClickListener
+            }
+            
+        }
     }
 }
