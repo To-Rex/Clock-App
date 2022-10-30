fbab4caed8591b6f81e1f64d98b2ca0fcdb8e8a3
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-30 18:51:23 +0500

    Calc Application Application

--- app/src/main/java/app/clock/alarmclock/cleint/UserService.kt
+++ app/src/main/java/app/clock/alarmclock/cleint/UserService.kt
@@ -1,24 +1,29 @@
 package app.clock.alarmclock.cleint
 
-import app.clock.alarmclock.models.GetTimes
 import retrofit2.http.POST
 import app.clock.alarmclock.models.LoginModels
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
 
 interface UserService {
     @POST("login")
     fun login(@Body loginModels: LoginModels?): Call<Any?>?
+
     @POST("register")
     fun register(@Body loginModels: LoginModels?): Call<Any?>?
+
     @POST("resendverefy")
     fun resendverefy(@Body loginModels: LoginModels?): Call<Any?>?
+
     @POST("verefyuser")
     fun verefyuser(@Body loginModels: LoginModels?): Call<Any?>?
+
     @POST("cheskverefy")
     fun cheskverefy(@Body loginModels: LoginModels?): Call<Any?>?
+
     @GET("gettimes")
-    fun gettimes(): Call<GetTimes>?
+    fun gettimes(@Header("Authorization") token: String?): Call<Any?>?
 
 }
