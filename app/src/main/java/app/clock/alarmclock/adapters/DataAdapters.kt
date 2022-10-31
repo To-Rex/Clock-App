ba6046a05079a3a255e32cbecb597ea138eec7e1
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-31 17:32:26 +0500

    Calc Application Application

--- app/src/main/java/app/clock/alarmclock/adapters/DataAdapters.kt
+++ app/src/main/java/app/clock/alarmclock/adapters/DataAdapters.kt
@@ -6,14 +6,8 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.BaseAdapter
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.Switch
-import android.widget.TextView
-import android.widget.TimePicker
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import app.clock.alarmclock.R
@@ -44,13 +38,19 @@ class DataAdapters(context: Context, timeList: ArrayList<GetTimes>) : BaseAdapte
         return position.toLong()
     }
 
-    @SuppressLint("ViewHolder", "UseSwitchCompatOrMaterialCode", "InflateParams", "MissingInflatedId")
+    @SuppressLint(
+        "ViewHolder",
+        "UseSwitchCompatOrMaterialCode",
+        "InflateParams",
+        "MissingInflatedId"
+    )
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val view = (context as Activity).layoutInflater.inflate(R.layout.times_list, null)
         val txtTime = view.findViewById<TextView>(R.id.txtTimes)
         val txtComents = view.findViewById<View>(R.id.txtComent) as TextView
         val switchItem = view.findViewById<View>(R.id.switchItem) as Switch
-        sharedPreferences = (context as Activity).getSharedPreferences("app.clock.alarmClock",
+        sharedPreferences = (context as Activity).getSharedPreferences(
+            "app.clock.alarmClock",
             AppCompatActivity.MODE_PRIVATE
         )
         val token = sharedPreferences?.getString("token", "")!!
@@ -72,41 +72,51 @@ class DataAdapters(context: Context, timeList: ArrayList<GetTimes>) : BaseAdapte
             val timePickerEtem = views.findViewById<TimePicker>(R.id.timePickerEtem)
             val progresEtemBar = views.findViewById<ProgressBar>(R.id.progresEtemBar)
 
-            progresEtemBar.visibility = View.VISIBLE
-            imgEtemDelete.visibility = View.GONE
-            imgEtemEdit.visibility = View.GONE
-            txtEtemComment.visibility = View.GONE
-            timePickerEtem.visibility = View.GONE
-
-
             timePickerEtem.setIs24HourView(true)
             txtEtemComment.text = timeList?.get(position)?.coments
             imgEtemDelete.setOnClickListener {
-                val deleteResponse: Call<Any?>? = ApiCleint().userService.deleteTime(position,"Bearer $token")
-                deleteResponse?.enqueue(object : retrofit2.Callback<Any?> {
-                    override fun onResponse(call: Call<Any?>, response: retrofit2.Response<Any?>) {
-                        if (response.isSuccessful) {
-                            Toast.makeText(context, "Deleted", Toast.LENGTH_SHORT).show()
-                            dialog.dismiss()
-                        } else {
-                            progresEtemBar.visibility = View.GONE
-                            imgEtemDelete.visibility = View.VISIBLE
-                            imgEtemEdit.visibility = View.VISIBLE
-                            txtEtemComment.visibility = View.VISIBLE
-                            timePickerEtem.visibility = View.VISIBLE
-                            Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
-                        }
-                    }
+                //delete dialog
+                AlertDialog.Builder(context as Activity)
+                    .setTitle("Diqqat!")
+                    .setMessage("Siz rostdan ham ushbu vaqtni o'chirmoqchimisiz?")
+                    .setPositiveButton("OK") { dialog, _ ->
+                        progresEtemBar.visibility = View.VISIBLE
+                        imgEtemDelete.visibility = View.GONE
+                        imgEtemEdit.visibility = View.GONE
+                        txtEtemComment.visibility = View.GONE
+                        timePickerEtem.visibility = View.GONE
+                        val deleteResponse: Call<Any?>? =
+                            ApiCleint().userService.deleteTime(position, "Bearer $token")
+                        deleteResponse?.enqueue(object : retrofit2.Callback<Any?> {
+                            override fun onResponse(call: Call<Any?>, response: retrofit2.Response<Any?>) {
+                                if (response.isSuccessful) {
+                                    Toast.makeText(context, "Deleted", Toast.LENGTH_SHORT).show()
+                                    dialog.dismiss()
+                                } else {
+                                    progresEtemBar.visibility = View.GONE
+                                    imgEtemDelete.visibility = View.VISIBLE
+                                    imgEtemEdit.visibility = View.VISIBLE
+                                    txtEtemComment.visibility = View.VISIBLE
+                                    timePickerEtem.visibility = View.VISIBLE
+                                    Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
+                                }
+                            }
 
-                    override fun onFailure(call: Call<Any?>, t: Throwable) {
-                        progresEtemBar.visibility = View.GONE
-                        imgEtemDelete.visibility = View.VISIBLE
-                        imgEtemEdit.visibility = View.VISIBLE
-                        txtEtemComment.visibility = View.VISIBLE
-                        timePickerEtem.visibility = View.VISIBLE
-                        Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
+                            override fun onFailure(call: Call<Any?>, t: Throwable) {
+                                progresEtemBar.visibility = View.GONE
+                                imgEtemDelete.visibility = View.VISIBLE
+                                imgEtemEdit.visibility = View.VISIBLE
+                                txtEtemComment.visibility = View.VISIBLE
+                                timePickerEtem.visibility = View.VISIBLE
+                                Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
+                            }
+                        })
+                        dialog.dismiss()
                     }
-                })
+                    .setNegativeButton("Bekor qilish") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show()
             }
 
             dialog.show()
