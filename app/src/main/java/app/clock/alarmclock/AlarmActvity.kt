1b6a0f84df5c85c87fd4c40b0816c51204c6d624
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-11-02 13:19:31 +0500

    Calc Application Application

--- app/src/main/java/app/clock/alarmclock/AlarmActvity.kt
+++ app/src/main/java/app/clock/alarmclock/AlarmActvity.kt
@@ -1,9 +1,11 @@
 package app.clock.alarmclock
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.Ringtone
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class AlarmActvity : AppCompatActivity() {
+    var ringtone: Ringtone? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm_actvity)
